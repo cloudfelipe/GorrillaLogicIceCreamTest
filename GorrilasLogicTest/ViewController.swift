d0bb7b3febd16180b4b7bf76516524ed29085c10
felipecondor <fcorrea@condorlabs.io>
2020-12-10 20:19:08 -0600

    Initial Commit

---
+++ GorrilasLogicTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GorrilasLogicTest
+//
+//  Created by Felipe Correa on 10/12/20.
+//  Copyright © 2020 Felipe & Co. Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
